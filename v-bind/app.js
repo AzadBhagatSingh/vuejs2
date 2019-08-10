6f0860093351251fa86ee22ee7206f3ffcc5a02e
AzadBhagatSingh <singhaazad@gmail.com>
2019-08-10 12:47:27 +0530

    html-attribute, class, input, htmlContent binding

---
+++ v-bind/app.js
@@ -0,0 +1,13 @@
+var MyView = new Vue({
+	el:"#myapp",
+	data:{
+		name: "Azad Bhagat Singh",
+		imageLink: "https://yt3.ggpht.com/a/AGF-l7_6bIQdFFn2MFB-0NTrpNINoSK6QnWidCAqjg=s288-c-k-c0xffffffff-no-rj-mo",
+		ulink: "https://www.youtube.com/channel/UCKpfU-Q0rIGqMzh5X016-xQ",
+		title: "Youtube Channel Link",
+		isActive:true,
+		txtName: "Azad Bhagat Singh",
+		txtEmail: "azad@gmail.com",
+		htmlContent:"<h2> This HTML form js file</h2>"
+	}
+});
